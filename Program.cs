b8cda96a3bc9e0695a198ecfea19da8001c7a0cc
Andrei <andruxametelkin@gmail.com>
2022-07-31 19:19:20 +0400

    Complete Homeworks

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Zadacha 64
+int Kolvochisel(int N)
+{
+    if (N / 10 != 0)
+        return 1 + Kolvochisel(N / 10);
+    else return 1;
+}
+Console.Write("Пожалуйста, введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количетсво цифр в указанном числе: " + Kolvochisel(n1));
+*/
+/* Zadacha 66 
+int Summa (int n, int m)
+{
+    if (n < m)
+    {
+        while (n != m)
+            return n + Summa(n + 1, m);
+    }
+
+    if (n > m)
+    {
+        while (n != m)
+            return n + Summa(n - 1, m);
+    }
+
+    return n;
+}
+
+Console.Write("Введите 1 число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число: ");
+int m1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Сумма натуральных элементов в указанном промежутке: " + Summa(n1, m1));
+*/
